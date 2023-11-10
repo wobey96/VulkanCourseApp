528a9c04ec55f41471b9b6cee2f50595af73c023
wobey96 <wobey96@gmail.com>
2023-11-10 01:59:41 -0700

    Still trying to get some things to work

---
+++ VulkanRenderer.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#define GLFW_INCLUDE_VULKAN 
+#include <GLFW\glfw3.h>
+
+
+#include <stdexcept>
+#include <vector>
+  
+class VulkanRenderer
+{
+	public:
+		VulkanRenderer();
+		~VulkanRenderer(); 
+};
+
--- main.cpp
+++ main.cpp
@@ -1,38 +1,27 @@
-#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
+#define GLFW_INCLUDE_VULKAN 
+#include <GLFW\glfw3.h>
 
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/glm.hpp>
-#include <glm/mat4x4.hpp>
+#include <vector>
+#include <stdexcept> 
+#include <iostream>
 
-#include <iostream>		
+/*
+GLFWwindow* window; 
+
+void initWindow(std::string wName = "Test Window", const int width = 800, const int height = 600)
+{
+	// Initialize GLFW
+	glfwInit(); 
+
+	// set glfw to not work with OpenGL
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); 
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+	window = glfwCreateWindow(width, height, wName.c_str(), nullptr, nullptr); 
+}
+*/
 
 int main()
 {
-	glfwInit(); 
-
-	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Test Window", nullptr, nullptr);
-		
-	uint32_t extensionCount = 0; 
-	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr); 
-
-	printf(" Extension count %i \n", extensionCount);  
-
-	glm::mat4 testMatrix(1.0f); 
-	glm::vec4 testVector(1.0f); 
-
-	auto testResult = testMatrix * testVector;
-
-	while (!glfwWindowShouldClose(window))
-	{
-		glfwPollEvents(); 
-	}
-
-	glfwDestroyWindow(window); 
-
-	glfwTerminate(); 
-
 	return 0; 
 }
